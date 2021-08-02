e4b70252f92c0ff5a9d393a63c99fc1e3ae28ba7
S-pl1ng <43790361+S-pl1ng@users.noreply.github.com>
2021-08-02 16:16:37 +0530

    Create re_assgn.py

---
+++ re_assgn.py
@@ -0,0 +1,11 @@
+import re
+
+file = open(r"C:\Users\Hp\python_web_course\actual_data.txt")
+total=0
+for line in file:
+    line=line.rstrip()
+    num_list = re.findall("[0-9]+", line)
+    if len(num_list)>0:
+        for i in range(len(num_list)):
+            total+=int(num_list[i])
+print("Total sum is: ", total)
